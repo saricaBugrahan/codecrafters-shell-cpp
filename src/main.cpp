37886be177eb817f472c2f805378bf42daac4c10
saricaBugrahan <saricabugrahan@gmail.com>
2024-06-09 17:22:14 +0300

    stage-1

--- src/main.cpp
+++ src/main.cpp
@@ -9,8 +9,8 @@ int main() {
   std::cout << "Logs from your program will appear here!\n";
 
   // Uncomment this block to pass the first stage
-  // std::cout << "$ ";
-  //
-  // std::string input;
-  // std::getline(std::cin, input);
+   std::cout << "$ ";
+
+   std::string input;
+   std::getline(std::cin, input);
 }
